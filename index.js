60e3a2b629083ee14f3e2156fdd2f141c1d8b9cb
Rob Loach <robloach@gmail.com>
2017-01-02 23:56:22 -0500

    Update to use robloach-datfile

--- index.js
+++ index.js
@@ -1,64 +1,38 @@
 var fs = require('fs')
 var path = require('path')
-var readline = require('readline')
 var acceptableExtensions = require('./lib/acceptableExtensions.json')
 var rejectNames = require('./lib/rejectNames.json')
 var rejectGames = require('./lib/rejectGames.json')
-var sortObject = require('sort-object')
+var sortArray = require('sort-array')
+var datfile = require('robloach-datfile')
 
-var lineReader = readline.createInterface({
-  input: fs.createReadStream('TDC.dat')
-});
-
-var database = {}
-var currentGame = ''
-
-lineReader.on('line', function (line) {
-	// New Game Name
-	if (line.indexOf('name "') >= 0) {
-		// Deny installer, trainers or cheats runnings.
-		for (var type in rejectGames) {
-			if (line.indexOf(rejectGames[type]) > 0) {
-				currentGame = null
-				return
-			}
+datfile.parseFile('TDC.dat', {ignoreHeader: true}).then(function (dat) {
+	let database = []
+	for (let i in dat) {
+		let game = dat[i]
+		let finalFile = null
+		for (let i in game.entries || []) {
+			let file = game.entries[i]
+			let cleanFilename = acceptableFile(file)
+	  		if (cleanFilename) {
+	  			file.cleanFilename = cleanFilename
+				finalFile = file
+	  		}
 		}
-
-		currentGame = cleanGameName(line)
-		database[currentGame] = {}
-	}
-	// File entry
-	else if (line.indexOf('file (') >= 0) {
-		// See if we are acting on the given game.
-		if (currentGame) {
-			// Clean up the file.
-			var file = cleanFile(line)
-			database[currentGame][file.name] = file
+	  	if (finalFile) {
+	  		database.push({
+	  			name: cleanGameName(game.name),
+	  			filename: finalFile.cleanFilename,
+	  			size: finalFile.size,
+	  			crc: finalFile.crc
+	  		})
 		}
 	}
-})
 
-function acceptableFile(file) {
-	if (file.pathObject.dirname) {
-		return false
-	}
-	var extension = file.pathObject.ext.toLowerCase()
-	if (acceptableExtensions.indexOf(extension) < 0) {
-		return false
-	}
+	database = sortArray(database, 'name')
 
-	var name = file.pathObject.name.toLowerCase()
-	if (rejectNames.indexOf(name) >= 0) {
-		return false
-	}
-
-	return true
-}
-
-lineReader.on('close', function () {
-	database = sortObject(database)
-  var pkg = require('./package.json')
-  var output = `clrmamepro (
+	var pkg = require('./package.json')
+	var output = `clrmamepro (
 	name "DOS"
 	description "DOS"
 	version ${pkg.version}
@@ -66,34 +40,43 @@ lineReader.on('close', function () {
 	homepage "${pkg.homepage}"
 )
 `
-  for (var name in database) {
-  	var finalFile = null
-  	var files = database[name]
-  	for (var filename in files) {
-  		var file = files[filename]
-  		if (acceptableFile(file)) {
-			finalFile = file
-  		}
-  	}
-
-  	if (finalFile) {
-	  	output += `
+	for (let i in database) {
+		let game = database[i]
+		output += `
 game (
-	name "${name}"
-	rom ( name "${finalFile.pathObject.base}" size ${finalFile.size} crc ${finalFile.crc} )
+	name "${game.name}"
+	rom ( name "${game.filename}" size ${game.size} crc ${game.crc} )
 )
 `
-  	}
-  }
-  fs.writeFileSync('DOS.dat', output)
+	}
+	fs.writeFileSync('DOS.dat', output)
+}).catch(function (err) {
+	console.error(err)
 })
 
-function cleanGameName(name) {
-	// Remove the prefixing "name".
-	var output = name.trim().replace('name "', '')
+function acceptableFile(file) {
+	var filename = file.name
+	filename = replaceAll(filename, '\\\\', '/')
+	var pathObject = path.parse(filename)
+	if (pathObject.dir) {
+		return false
+	}
+	var extension = pathObject.ext.toLowerCase()
+	if (acceptableExtensions.indexOf(extension) < 0) {
+		return false
+	}
 
+	var name = pathObject.name.toLowerCase()
+	if (rejectNames.indexOf(name) >= 0) {
+		return false
+	}
+
+	return pathObject.base
+}
+
+function cleanGameName(name) {
 	// Remove the suffixing file extension.
-	output = output.replace('.zip"', '')
+	output = name.replace('.zip"', '')
 
 	// Remove the version.
 	//var ver = output.match(/\ v[0-9.]{3,9}\ /g)
@@ -124,36 +107,4 @@ function cleanGameName(name) {
 
 function replaceAll(str, find, replace) {
   return str.replace(new RegExp(find, 'g'), replace);
-}
-
-function cleanFile(file) {
-	var output = file.trim()
-	output = output.replace('file ( ', '')
-
-    // Build a regex to find the file information.
-	var regex = /name (.*) size (\S*) date (\S*) (\S*) crc (\S*) \)/g
-	var match = regex.exec(output)
-
-	// Error out if the regex failed.
-	if (!match) {
-		console.error('Failed to regex match ' + file)
-		process.exit(1)
-	}
-
-	// Get the filename.
-	var filename = match[1]
-
-	// Replace Windows directory seperators with Unix /.
-	filename = filename.split('\\').join('/')
-
-	// Construct a path object.
-	var pathObj = path.parse(filename)
-
-	// Return the new file object.
-	return {
-		name: pathObj.base,
-		size: match[2],
-		crc: match[5],
-		pathObject: pathObj
-	}
 }
